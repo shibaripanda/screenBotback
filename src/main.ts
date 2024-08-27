e45a58727dfc2c8a12031cf5659c8d986f52970b
panda <baradach.jsdev@gmail.com>
2024-08-28 00:27:29 +0300

    dfdf

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+
+  const PORT = process.env.PORT || 5002
+  const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  })
+
+  await app.listen(PORT, () => {console.log(`Server started on port = ${PORT}`)})
+  
+}
+bootstrap();
